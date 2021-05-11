d8e1edad2fafd85a0306ce24d3d6590b35086d56
StephanGuter <51406643+StephanGuter@users.noreply.github.com>
2021-05-11 14:24:53 -0300

    Creating a custom board exception

--- Xadrez-Console/Board/Board.cs
+++ Xadrez-Console/Board/Board.cs
@@ -18,10 +18,41 @@
             return pieces[line, column];
         }
 
+        public Piece Piece(Position position)
+        {
+            return pieces[position.line, position.column];
+        }
+
+        public bool PieceExists(Position pos)
+        {
+            ValidatePosition(pos);
+            return Piece(pos) != null;
+        } 
+
         public void PlacePiece(Piece p, Position pos)
         {
             pieces[pos.line, pos.column] = p;
             p.position = pos;
+        }
+
+        public bool IsValidPosition(Position pos)
+        {
+            if (pos.line < 0 || pos.line >= lines || pos.column < 0 || pos.column >= columns)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void ValidatePosition(Position pos)
+        {
+            if (!IsValidPosition(pos))
+            {
+                throw new BoardException("Posição inválida!");
+            }
         }
     }
 }
