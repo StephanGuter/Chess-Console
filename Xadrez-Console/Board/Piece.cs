a96679c21896c41387eead7ca7088407e85f44fb
StephanGuter <51406643+StephanGuter@users.noreply.github.com>
2021-05-11 14:03:45 -0300

    Starting to place the pieces on the board

--- Xadrez-Console/Board/Piece.cs
+++ Xadrez-Console/Board/Piece.cs
@@ -2,16 +2,14 @@
 {
     class Piece
     {
-        public Chessman chessman { get; protected set; }
         public Position position { get; set; }
         public Color color { get; protected set; }
         public Board board { get; protected set; }
         public int MovementAmount { get; protected set; }
 
-        public Piece(Chessman chessman, Position position, Color color, Board board)
+        public Piece(Color color, Board board)
         {
-            this.chessman = chessman;
-            this.position = position;
+            this.position = null;
             this.color = color;
             this.board = board;
             MovementAmount = 0;
--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using board;
+using chess;
 
 namespace chess_console
 {
@@ -7,9 +8,13 @@ namespace chess_console
     {
         static void Main(string[] args)
         {
-            Board B = new Board(8, 8);
+            Board b = new Board(8, 8);
 
-            Display.printBoard(B);
+            b.PlacePiece(new King(Color.Black, b), new Position(1, 3));
+            b.PlacePiece(new King(Color.White, b), new Position(3, 5));
+            b.PlacePiece(new Queen(Color.White, b), new Position(5, 6));
+
+            Display.printBoard(b);
         }
     }
 }
